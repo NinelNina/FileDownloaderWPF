d0b847dad3aad0039e8aa51807beb66f1d91e5b5
Nina <ninkr@list.ru>
2020-12-01 13:09:28 +0300

    Add graphic interface

---
+++ FileDownloaderWPF/DownloadWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using FileDownloaderConsole;
+
+namespace FileDownloaderWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для DownloadWindow.xaml
+    /// </summary>
+    public partial class DownloadWindow : Window
+    {
+        //public string sourcePath;
+        //public string destinationPath;
+
+        private int fileDownloadedCount = 0;
+        private int fileUndownloadedCount = 0;
+        private double downloadedFiles;
+        private double numberOfFiles;
+        public InputData inputData;
+        public DownloadWindow(string sourcePath, string destinationPath)
+        {
+            inputData = new InputData();
+
+            inputData.PathToOpen = sourcePath;
+            inputData.PathToSave = destinationPath;
+
+            InitializeComponent();
+
+            BeginDownloading();
+        }
+
+        private void BeginDownloading()
+        {
+            inputData.Input();
+            numberOfFiles = inputData.numberOfFiles;
+
+            FileDownloader fileDownloader = new FileDownloader();
+
+            CountDownloadingFiles downloadingFiles = new CountDownloadingFiles();
+
+            fileDownloader.OnDownloaded += CountDownloadedFiles;
+            fileDownloader.OnFailed += CountUndownloadedFiles;
+
+            int index = 1;
+
+            try
+            {
+                foreach (string url in inputData.fileUrls)
+                {
+
+                    fileDownloader.AddFileToDownloadingQueue(Convert.ToString(index), url, inputData.PathToSave);
+                    index++;
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.WriteToLog(exception);
+                MessageBox.Show("Ошибка!" + exception.Message);
+
+                return;
+            }
+        }
+
+        public void CountDownloadedFiles(string message)
+        {
+            fileDownloadedCount++;
+
+            downloadedFiles = (Convert.ToDouble(fileDownloadedCount) / Convert.ToDouble(numberOfFiles)) * 100;
+            downloadedFiles = (int)Math.Round(downloadedFiles);
+
+            MessageBox.Show($"Загрузка файла <<{message}>> {downloadedFiles}%");
+
+            ShowNumberOfDownloadedFiles();
+        }
+
+        private void ShowNumberOfDownloadedFiles()
+        {
+            if (fileDownloadedCount + fileUndownloadedCount == numberOfFiles)
+            {
+                MessageBox.Show($"Загружено файлов: {fileDownloadedCount}. Загружено с ошибками: {fileUndownloadedCount}");
+            }
+        }
+
+        public void CountUndownloadedFiles(string message, Exception exception)
+        {
+            fileUndownloadedCount++;
+
+            Log.WriteToLog(message, exception);
+
+            ShowNumberOfDownloadedFiles();
+        }
+    }
+}
---
+++ FileDownloaderWPF/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using FileDownloaderConsole;
+//using Microsoft.Win32;
+
+namespace FileDownloaderWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string sourcePath;
+        string destinationPath;
+        //public InputData inputData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            inputPathTextBox.TextChanged += InputPathTextBox_TextChanged;
+            outputPathTextBox.TextChanged += OutputPathTextBox_TextChanged;
+            
+            outputPath.IsEnabled = false;
+            outputPathTextBox.IsEnabled = false;
+
+            downloadButton.IsEnabled = false;
+        }
+
+        private void InputPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+
+            dialog.DefaultExt = "txt";
+            dialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                inputPathTextBox.Text = dialog.FileName;
+                sourcePath = inputPathTextBox.Text;
+
+                outputPath.IsEnabled = true;
+                outputPathTextBox.IsEnabled = true;
+            }
+        }
+
+        private void OutputPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                outputPathTextBox.Text = dialog.SelectedPath;
+                destinationPath = outputPathTextBox.Text;
+                downloadButton.IsEnabled = true;
+            }
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DownloadWindow downloadWindow = new DownloadWindow(sourcePath, destinationPath);
+                downloadWindow.Show();
+
+                Close();
+            }
+            catch (Exception exception)
+            {
+               Log.WriteToLog(exception);
+               MessageBox.Show("Ошибка!" + exception.Message);
+            }
+        }
+
+        private void Clear()
+        {
+            inputPathTextBox.Text = "";
+
+
+            outputPathTextBox.Text = "";
+        }
+
+        private void InputPathTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            sourcePath = inputPathTextBox.Text;
+        }
+
+        private void OutputPathTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            destinationPath = outputPathTextBox.Text;
+        }
+    }
+}

fd38a36affacfb2bcdb768254642722aa7a6d875
Nina <ninkr@list.ru>
2020-12-07 18:21:43 +0300

    Add file download progress

--- FileDownloaderWPF/DownloadWindow.xaml.cs
+++ FileDownloaderWPF/DownloadWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,38 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using FileDownloaderConsole;
 
 namespace FileDownloaderWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для DownloadWindow.xaml
-    /// </summary>
     public partial class DownloadWindow : Window
     {
-        //public string sourcePath;
-        //public string destinationPath;
-
         private int fileDownloadedCount = 0;
         private int fileUndownloadedCount = 0;
-        private double downloadedFiles;
+        private int fullDownloadProgress = 0;
+
         private double numberOfFiles;
         public InputData inputData;
+        private List<FileData> items;
+
+        void fullProgressWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            do
+            {
+                (sender as BackgroundWorker).ReportProgress(fullDownloadProgress);
+                System.Threading.Thread.Sleep(100);
+
+            } while (fullDownloadProgress <= 100);
+        }
+
+        void fullProgressWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            fullProgress.Value = e.ProgressPercentage;
+        }
+
         public DownloadWindow(string sourcePath, string destinationPath)
         {
             inputData = new InputData();
@@ -44,13 +58,22 @@ namespace FileDownloaderWPF
         {
             inputData.Input();
             numberOfFiles = inputData.numberOfFiles;
+            
+            items = new List<FileData>();
 
             FileDownloader fileDownloader = new FileDownloader();
 
-            CountDownloadingFiles downloadingFiles = new CountDownloadingFiles();
-
             fileDownloader.OnDownloaded += CountDownloadedFiles;
             fileDownloader.OnFailed += CountUndownloadedFiles;
+            fileDownloader.OnFileProgress += CountReadBytes;
+
+            BackgroundWorker fullProgressWorker = new BackgroundWorker();
+            fullProgressWorker.WorkerReportsProgress = true;
+            fullProgressWorker.DoWork += fullProgressWorker_DoWork;
+            fullProgressWorker.ProgressChanged += fullProgressWorker_ProgressChanged;
+
+            fullProgressWorker.RunWorkerAsync();
+
 
             int index = 1;
 
@@ -60,6 +83,12 @@ namespace FileDownloaderWPF
                 {
 
                     fileDownloader.AddFileToDownloadingQueue(Convert.ToString(index), url, inputData.PathToSave);
+
+                    string fileName = FileExtension.GetFileName(url);
+
+                    items.Add(new FileData() {FileID = Convert.ToString(index), FileName = fileName, Size = 0, Progress = 0 });
+                    fileList.ItemsSource = items;
+
                     index++;
                 }
             }
@@ -72,33 +101,73 @@ namespace FileDownloaderWPF
             }
         }
 
-        public void CountDownloadedFiles(string message)
+        public void CountDownloadedFiles(string fileId)
         {
             fileDownloadedCount++;
 
-            downloadedFiles = (Convert.ToDouble(fileDownloadedCount) / Convert.ToDouble(numberOfFiles)) * 100;
-            downloadedFiles = (int)Math.Round(downloadedFiles);
-
-            MessageBox.Show($"Загрузка файла <<{message}>> {downloadedFiles}%");
-
             ShowNumberOfDownloadedFiles();
+
+            foreach (var item in items)
+            {
+                if (item.FileName.Contains(fileId))
+                {
+                    if (item.Size == 0)
+                    {
+                        item.Progress = 100;
+                    }
+
+                    return;
+                }
+            }
         }
 
         private void ShowNumberOfDownloadedFiles()
         {
-            if (fileDownloadedCount + fileUndownloadedCount == numberOfFiles)
+            double downloadedFilesTemp = (Convert.ToDouble(fileDownloadedCount + fileUndownloadedCount) / Convert.ToDouble(numberOfFiles)) * 100;
+            fullDownloadProgress = (int)Math.Round(downloadedFilesTemp);
+
+            if (fullDownloadProgress == 100)
             {
-                MessageBox.Show($"Загружено файлов: {fileDownloadedCount}. Загружено с ошибками: {fileUndownloadedCount}");
+                MessageBox.Show($"Загружено файлов: {fileDownloadedCount}. Загружено с ошибками: {fileUndownloadedCount}", "Загрузка выполнена", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
-        public void CountUndownloadedFiles(string message, Exception exception)
+        public void CountUndownloadedFiles(string fileId, Exception exception)
         {
             fileUndownloadedCount++;
 
-            Log.WriteToLog(message, exception);
+            Log.WriteToLog(fileId, exception);
 
             ShowNumberOfDownloadedFiles();
+        }
+
+        public void CountReadBytes(string fileId, int totalValue, int downloadedBytes)
+        {
+            int progress = 1;
+
+            if (totalValue != 0)
+            {
+                double downloadedBytesTemp = downloadedBytes / totalValue;
+                progress = (int)Math.Round(downloadedBytesTemp * 100);
+            }
+
+            foreach (var item in items)
+            {
+                if (item.FileID.Contains(fileId))
+                {
+                    if (item.Size != 0)
+                    {
+                        item.Progress = progress;
+                    }
+                    else
+                    {
+                        item.Size = totalValue;
+                        item.Progress = progress;
+                    }
+
+                    return;
+                }
+            }
         }
     }
 }
---
+++ FileDownloaderWPF/FileData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloaderWPF
+{
+    public class FileData : INotifyPropertyChanged
+    {
+        string fileNameValue;
+        int sizeValue;
+        int progressValue;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string FileID { set; get; }
+        public string FileName
+        {
+            set
+            {
+                if (value != fileNameValue)
+                {
+                    fileNameValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            get
+            {
+                return fileNameValue;
+            }
+        }
+        public int Size
+        {
+            set
+            {
+                if (value != sizeValue)
+                {
+                    sizeValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            get
+            {
+                return sizeValue;
+            }
+        }
+        public int Progress
+        {
+            set
+            {
+                if (value != progressValue)
+                {
+                    progressValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            get
+            {
+                return progressValue;
+            }
+        }
+
+    }
+}
